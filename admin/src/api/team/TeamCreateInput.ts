67f748895f5d7aeb88c13661b1944e1091a46dbd
David Villlegas <davidvillegas15@gmail.com>
2021-03-11 02:07:50 +0100

    amplication-build-ckm46949989968710s6i20m979c

--- admin/src/api/team/TeamCreateInput.ts
+++ admin/src/api/team/TeamCreateInput.ts
@@ -1,6 +1,9 @@
 import { CountryWhereUniqueInput } from "../country/CountryWhereUniqueInput";
+import { MatchWhereUniqueInput } from "../match/MatchWhereUniqueInput";
 
 export type TeamCreateInput = {
   country?: CountryWhereUniqueInput | null;
   league?: string | null;
+  matches?: MatchWhereUniqueInput | null;
+  visitor?: MatchWhereUniqueInput;
 };
